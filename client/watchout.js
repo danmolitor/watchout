26a5acb4b1147103c44be30c652309203f37af79
danmolitor <danmolitor91@gmail.com>
2016-01-22 16:07:02 -0800

    initial commit

---
+++ client/watchout.js
@@ -0,0 +1,15 @@
+// start slingin' some d3 here.
+
+//create gameBoard object, with dimensions.
+var gameBoard = {
+  height : 800,
+  width :  750,
+};
+
+//create svg variable as gameBoard
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", gameBoard.width)
+            .attr("height", gameBoard.height)
+            .classed("svg", true)
+            .style("background-color", "black");
